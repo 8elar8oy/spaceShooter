e5e406d84cd6b04940ee062b087578aea4ee53bd
MaksimKhoruzhenko <114007431+8elar8oy@users.noreply.github.com>
2023-03-07 18:14:04 +0300

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "settings.h"
+#include "player.h"
+class Game {
+private:
+	sf::RenderWindow window;
+	Player player;
+	void checkEvents() {
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)	window.close();
+		}
+	}
+	void update() {
+		player.update();
+	}
+	void checkCollisions() {}
+	void draw() {
+		window.clear();
+		window.draw(player.getSprite());
+		window.display();
+	}
+public:
+	Game() {
+		window.create(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
+		window.setFramerateLimit(FPS);
+	}
+	void play() {
+		while (window.isOpen())
+		{
+			checkEvents();
+			update(   );
+			checkCollisions();
+			draw(   );
+		}
+	}
+
+};
---
+++ player.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "settings.h"
+
+class Player {
+private:
+	sf::Sprite sprite;
+	sf::Texture texture;
+public:
+	Player() {
+		//sprite =
+	}
+	void update() {}
+	sf::Sprite getSprite() {
+		return sprite;
+	}
+
+};
