2fa62fcc00cb13f4ecd8a14a534aecc3ed77acd3
MaksimKhoruzhenko <114007431+8elar8oy@users.noreply.github.com>
2023-03-25 14:27:32 +0300

    Delete settings.h

--- settings.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "SFML/Graphics.hpp"
-using namespace sf;
-//окно
-const float WINDOW_WIDTH = 1920.f;
-const float WINDOW_HEIGHT = 1080.f;
-const std::string WINDOW_TITLE = "SFML RACING";
-const float FPS = 60;
